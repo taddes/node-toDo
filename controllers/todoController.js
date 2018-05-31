52412c539c8070a667bcd7821ba517f48e056381
Taddes Korris <taddes.korris@gmail.com>
2018-05-30 21:06:26 -0400

    routes set

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,16 @@
 module.exports = function(app){
+
+  app.get('/todo', (req, res) => {
+    res.render
+  });
+
+  app.post('/todo', (req, res) => {
+
+  });
   
-  
+  app.delete('/todo', (req, res) => {
+
+
+  });
 
 };
