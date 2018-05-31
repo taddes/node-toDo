2b0170e0de72f142b1185c5cfb1d3b6f2e944475
Taddes Korris <taddes.korris@gmail.com>
2018-05-31 15:07:09 -0400

    configured schema

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,19 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Connect to database (MongoDB via mLab)
+mongoose.connect('mongodb://<test>:<password1>@ds157571.mlab.com:57571/node-todo');
+
+// Schema
+const todoSchema = new mongoose.Schema({
+  item: String
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'draw cats'}).save((err) => {
+  if (err) throw err;
+  console.log('item saved');
+})
 
 const data = [{item: 'groceries'}, {item: 'play with the cat'}, {item:'exercise'}];
 const urlencodedParser = bodyParser.urlencoded({extended: false});
